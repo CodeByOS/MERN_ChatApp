9dafdfdd42b977082986137fb4bef1c6e37d3ee4
Oussama <saidi.oussama2004@gmail.com>
2025-05-22 21:43:37 +0100

    Updates..

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -21,8 +21,8 @@ const ProfilePage = () => {
     };
   };
   return (
-    <div className="h-screen pt-20">
-      <div className="max-w-2xl mx-auto p-4 py-3">
+    <div className="h-full pt-20">
+      <div className="max-w-2xl mx-auto p-4 py-3 rounded-lg shadow-lg">
         <div className="bg-base-300 rounded-xl p-6 space-y-8">
 
           {/* HEADER OF THE PROFILE PAGE */}
